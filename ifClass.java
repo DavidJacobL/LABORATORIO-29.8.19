fe23520addda90c78400d5b69708b80ff058ac86
DavidJacobL <53485137+DavidJacobL@users.noreply.github.com>
2019-09-06 23:37:09 -0600

    Update ifClass.java

--- ifClass.java
+++ ifClass.java
@@ -13,7 +13,7 @@ public class ifClass {
     public ifClass() {
     }
 
-    int n1 = 0, n2 = 0;
+    int n1 = 0, n2 = 0, n3 = 0;
 
     //METODO setIgual
     public void setIgual(int x, int y) {
@@ -28,6 +28,7 @@ public class ifClass {
 
     }
 
+    //METODO SET MAYOR
     public void setMayor(int x, int y) {
         this.n1 = x;
         this.n2 = y;
@@ -39,6 +40,7 @@ public class ifClass {
 
     }
 
+    //METODO SET MENOR
     public void setMenor(int x, int y) {
         this.n1 = x;
         this.n2 = y;
@@ -49,6 +51,7 @@ public class ifClass {
         }
     }
 
+    //METODO SET MAXIMO
     public void setMaximo(int x, int y) {
         this.n1 = x;
         this.n2 = y;
@@ -74,6 +77,7 @@ public class ifClass {
         System.out.println("Numero Minimo: " + min);
     }
 
+    //METODO SET MINIMO
     public void setMinimo(int x, int y) {
         this.n1 = x;
         this.n2 = y;
@@ -98,62 +102,113 @@ public class ifClass {
         System.out.println("Numero Minimo: " + min);
     }
 
+    //RAIZ CUADRADA
     public void setsqrt(int x) {
         this.n1 = x;
-        int cuadrado = 0;
-            
-        
-        
-            System.out.println("La Raíz Cuadrada de: " +  x + "es: " + cuadrado);
+        System.out.println("Ingrese el radicando: ");
+        Scanner in = new Scanner(System.in);
+        float cuadrado = (float) Math.sqrt(n1);
 
-        }
-
-    
-    
-    
-    
-    
-
-    public void setfnCuadratica(int x, int y) {
-        this.n1 = x;
-        this.n2 = y;
+        System.out.println("La Raíz Cuadrada de: " + x + "es: " + cuadrado);
 
     }
 
+    //FUNCIÓN CUADRÁTICA
+    public void setfnCuadratica(int a, int b, int c) {
+        this.n1 = a;
+        this.n2 = b;
+        this.n3 = c;
+
+        System.out.println("\nFórmula Cuadrática");
+        double x1, x2, potencia, raiz = 0;
+
+        potencia = Math.pow(b, 2) - (4 * a * c);
+
+        x1 = (-b - Math.sqrt(potencia) / 2 * a);
+        x2 = (-b + Math.sqrt(potencia) / 2 * a);
+
+        System.out.println("\nX1 = " + x1);
+        System.out.println("\nX2 = " + x2);
+//INTENTAR PEDIR DATOS EN EL MAIN, A TRAVES DE SCANNER
+
+    }
+
+//FUNCIÓN PARA LA RESTA
     public void setResta(int x, int y) {
         this.n1 = x;
         this.n2 = y;
+        int resta = 0;
+        // resta = n1 - n2;
+
+        if (n1 < n2) {
+            throw new ArithmeticException("Números incorrectos - El minuendo debe ser MAYOR que el sustraendo");
+        } else {
+            resta = n1 - n2;
+            System.out.println(x + " - " + y + "= " + resta);
+        }
 
     }
 
+    //MULTIPLICACIÒN
     public void setMultiplicacion(int x, int y) {
         this.n1 = x;
         this.n2 = y;
-
+        int multi = 0;
+        multi = n1 * n2;
+        System.out.println(x + " x " + y + "  es igual a: " + multi);
     }
 
+    //METODO DE DIVISION
     public void setDivision(int x, int y) {
         this.n1 = x;
         this.n2 = y;
+        float division = 0;
+        // division = n1 / n2;
+
+        if (n1 < n2) {
+            throw new ArithmeticException("Números incorrectos - El dividendo debe ser MAYOR que el divisor");
+        } else {
+            division = n1 / n2;
+            System.out.println(x + " / " + y + "= " + division);
+        }
 
     }
-
+    
+    //METODO &&
     public void setAND(int x, int y) {
         this.n1 = x;
         this.n2 = y;
 
+        if (x == 8 && y == 2) {
+
+            System.out.println("TRUE");
+        } else {
+            System.out.println("FALSE. ADIOS MUNDO");
+        }
     }
 
-    public void setOR(int x, int y) {
+    //METODO ||
+    public void setO(int x, int y) {
         this.n1 = x;
         this.n2 = y;
+  if (x == 10 ||  y == 4) {
 
+            System.out.println("TRUE");
+        } else {
+            System.out.println("FALSE.NO HAY CONINCIDENCIAS");
+        }
     }
-
+    
+    
     public void setNOT(int x, int y) {
         this.n1 = x;
         this.n2 = y;
-
+if ( x != y ){
+    System.out.println("EL PRIMER NUMERO NO ES COMO EL SEGUNDO");
+}
+else{
+    System.out.println("EL PRIMER NUMERO ES COMO EL SEGUNDO");}
+    
     }
 
 }
